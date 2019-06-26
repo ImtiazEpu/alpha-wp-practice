57c0f53861d620b43d18f250124385b15f22d956
Imtiaz Epu <imtiazepu@programmer.net>
2019-06-27 05:08:46 +0600

    use WP_Query class

---
+++ cq-wpq.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Template Name: Custom Query WP_Query
+ */
+?>
+<?php get_header(); ?>
+<body <?php body_class(); ?>>
+<?php get_template_part( "/template-parts/common/hero" ); ?>
+    <div class="posts text-center">
+        <h2 class="post-title text-center mt-5">
+			<?php the_title(); ?>
+        </h2>
+		<?php
+		$paged          = get_query_var( "paged" ) ? get_query_var( "paged" ) : 1;
+		$posts_per_page = 3;
+		$post_ids       = array( 22, 23, 29, 16, 18, 32, 34, 41, 14, 37, 25, 1, 27, 39 );
+		$_p             = new WP_Query( array(
+			'post__in'       => $post_ids,
+			'orderby'        => 'post__in',
+			'posts_per_page' => $posts_per_page,
+			'paged'          => $paged
+
+		) );
+		 while ($_p->have_posts()){
+            $_p->the_post();
+			?>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<?php
+		}
+        wp_reset_query();
+		?>
+        <div class="container post-pagination">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-8">
+					<?php
+
+					?>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
