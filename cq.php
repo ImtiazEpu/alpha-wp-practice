c6a7078eb9861aaad2c572c782932182aa5cc095
Imtiaz Epu <imtiazepu@programmer.net>
2019-06-27 04:51:51 +0600

    custom query without 'setup_postdata()'

--- cq.php
+++ cq.php
@@ -13,24 +13,23 @@
 		<?php
 		$paged          = get_query_var( "paged" ) ? get_query_var( "paged" ) : 1;
 		$posts_per_page = 3;
-		$total = 15;
 		$post_ids       = array( 22, 23, 29, 16, 18, 32, 34, 41, 14, 37, 25, 1, 27, 39 );
 		$_p             = get_posts( array(
-			//'post__in'       => $post_ids,
-            'author__in'=>array(1),
+			'post__in'       => $post_ids,
 			'orderby'        => 'post__in',
-			'numberposts'=>$total,
 			'posts_per_page' => $posts_per_page,
 			'paged'          => $paged
 
 		) );
-		foreach ( $_p as $post ) {
-			setup_postdata( $post );
+		foreach ( $_p as $p ) {
+			//setup_postdata( $post );
 			?>
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <h3><a href="<?php echo esc_url($p->guid); ?>"><?php echo apply_filters("the_title",$p->post_title);
+                    ?></a></h3>
+<!--            <h3><a href="--><?php //the_permalink(); ?><!--">--><?php //the_title(); ?><!--</a></h3>-->
 			<?php
 		}
-		wp_reset_postdata();
+		//wp_reset_postdata();
 		?>
         <div class="container post-pagination">
             <div class="row">
@@ -38,7 +37,7 @@
                 <div class="col-md-8">
 					<?php
 					echo paginate_links( array(
-						'total' => ceil( $total / $posts_per_page )
+						'total' => ceil( count( $post_ids ) / $posts_per_page )
 					) );
 					?>
                 </div>
