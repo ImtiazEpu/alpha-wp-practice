ec2526c98fe6124d59c6895eaa11bff6cb73b45f
Imtiaz Epu <imtiazepu@programmer.net>
2019-07-04 02:58:22 +0600

    acf-create metabox in the admin panel

--- single.php
+++ single.php
@@ -2,8 +2,8 @@
 $alpha_layout_class = "col-md-8";
 $alpha_text_class   = "";
 if ( ! is_active_sidebar( "sidebar-1" ) ) {
-    $alpha_layout_class = "col-md-10 offset-md-1";
-    $alpha_text_class   = "text-center";
+	$alpha_layout_class = "col-md-10 offset-md-1";
+	$alpha_text_class   = "text-center";
 }
 ?>
 
@@ -14,121 +14,142 @@ if ( ! is_active_sidebar( "sidebar-1" ) ) {
         <div class="row">
             <div class="<?php echo $alpha_layout_class; ?> pt-4">
                 <div class="posts" <?php ?>>
-                    <?php
-                    while ( have_posts() ) :
-                        the_post();
-                        ?>
+					<?php
+					while ( have_posts() ) :
+						the_post();
+						?>
                         <div <?php post_class( array( "first_class", "second_class" ) ); ?>>
                             <div class="container">
                                 <div class="row">
                                     <div class="col-md-12">
                                         <h2 class="post-title <?php echo $alpha_text_class; ?>">
-                                            <?php the_title(); ?>
+											<?php the_title(); ?>
                                         </h2>
                                         <p class="<?php echo $alpha_text_class; ?>">
                                             <em><?php the_author_posts_link(); ?></em><br/>
-                                            <?php echo get_the_date(); ?>
+											<?php echo get_the_date(); ?>
                                         </p>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <!--<div class="slider">
-                                            <?php
-/*                                            if ( class_exists( 'Attachments' ) ) {
-                                                $attachments = new Attachments( 'slider' );
-                                                if ( $attachments->exist() ) {
-                                                    while ( $attachment = $attachments->get() ) { */?>
-                                                        <div>
-                                                            <?php /*echo $attachments->image( 'large' ); */?>
-                                                        </div>
-                                                        <?php
-/*                                                    }
-                                                }
-                                            }
-                                            */?>
-                                        </div>-->
                                         <div>
-                                            <?php
-                                            /*if ( !class_exists( 'Attachments' ) ) {*/
-                                                if ( has_post_thumbnail() ) {
-                                                    $thumbnail_url = get_the_post_thumbnail_url( null, "large" );
-                                                    printf( '<a class="popup" href="%s" data-featherlight="image">', $thumbnail_url );
-                                                    the_post_thumbnail( "large", array( "class" => "img-fluid mb-4" ) );
-                                                    echo '</a>';
-                                                /*}*/
-                                            }
-                                                //the_post_thumbnail("alpha-square-new1");
-                                                //the_post_thumbnail("alpha-square-new2");
-                                                //the_post_thumbnail("alpha-square-new3");
+											<?php
+											/*if ( !class_exists( 'Attachments' ) ) {*/
+											if ( has_post_thumbnail() ) {
+												$thumbnail_url = get_the_post_thumbnail_url( null, "large" );
+												printf( '<a class="popup" href="%s" data-featherlight="image">',
+													$thumbnail_url );
+												the_post_thumbnail( "large", array( "class" => "img-fluid mb-4" ) );
+												echo '</a>';
+												/*}*/
+											}
+											//the_post_thumbnail("alpha-square-new1");
+											//the_post_thumbnail("alpha-square-new2");
+											//the_post_thumbnail("alpha-square-new3");
 
-                                            the_content();
-                                            wp_link_pages();
-
-                                            ?>
+											the_content();
+											if ( get_post_format() == 'image' ):
+												?>
+                                                <div class="col-md-4 px-0">
+                                                    <ul class="list-group">
+                                                        <li class="list-group-item">
+                                                            <Strong>Camera Model:</Strong>
+															<?php $alpha_camera_model = get_field( "camera_model" );
+															echo esc_html( $alpha_camera_model );
+															?>
+                                                        </li>
+                                                        <li class="list-group-item">
+                                                            <Strong>Location:</Strong>
+															<?php $alpha_location = get_field( "location" );
+															echo esc_html( $alpha_location );
+															?>
+                                                        </li>
+                                                        <li class="list-group-item">
+                                                            <Strong>Date:</Strong>
+															<?php $alpha_date = get_field( "date" );
+															echo esc_html( $alpha_date );
+															?>
+                                                        </li>
+														<?php if ( get_field( "licen" ) ): ?>
+                                                            <li class="list-group-item">
+                                                                <Strong>Licensed information:</Strong>
+																<?php
+																$alpha_licensed_information = apply_filters( "the_content",
+																	get_field( "licensed_information" ) );
+																echo $alpha_licensed_information;
+																?>
+                                                            </li>
+														<?php endif; ?>
+                                                    </ul>
+                                                </div>
+											<?php
+											endif;
+											wp_link_pages();
+											?>
                                         </div>
                                     </div>
 
                                     <div class="authorsection mt-4">
                                         <div class="row">
                                             <div class="col-md-2 authorimage">
-                                                <?php
-                                                echo get_avatar( get_the_author_meta( "ID" ) );
-                                                ?>
+												<?php
+												echo get_avatar( get_the_author_meta( "ID" ) );
+												?>
                                             </div>
                                             <div class="col-md-10 pl-5">
                                                 <h4>
-                                                    <?php echo get_the_author_meta( "display_name" ); ?>
+													<?php echo get_the_author_meta( "display_name" ); ?>
                                                 </h4>
                                                 <p>
-                                                    <?php echo get_the_author_meta( "description" ); ?>
+													<?php echo get_the_author_meta( "description" ); ?>
                                                 </p>
 
                                             </div>
                                         </div>
                                     </div>
 
-                                    <?php if (!post_password_required()): ?>
+									<?php if ( ! post_password_required() ): ?>
                                         <div class="col-md-12">
-                                            <?php
-                                            comments_template();
-                                            ?>
+											<?php
+											comments_template();
+											?>
                                         </div>
-                                    <?php endif; ?>
+									<?php endif; ?>
                                 </div>
                             </div>
                         </div>
-                    <?php
-                    endwhile;
-                    ?>
+					<?php
+					endwhile;
+					?>
 
                     <div class="container post-pagination">
                         <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-8">
-                                <?php
-                                the_posts_pagination( array(
-                                    "screen_reader_text" => ' ',
-                                    "prev_text"          => "New Posts",
-                                    "next_text"          => "Old Posts"
-                                ) );
-                                ?>
+								<?php
+								the_posts_pagination( array(
+									"screen_reader_text" => ' ',
+									"prev_text"          => "New Posts",
+									"next_text"          => "Old Posts"
+								) );
+								?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <?php
-            if ( is_active_sidebar( "sidebar-1" ) ):
-                ?>
+			<?php
+			if ( is_active_sidebar( "sidebar-1" ) ):
+				?>
                 <div class="col-md-4">
-                    <?php
-                    if ( is_active_sidebar( "sidebar-1" ) ) {
-                        dynamic_sidebar( "sidebar-1" );
-                    }
-                    ?>
+					<?php
+					if ( is_active_sidebar( "sidebar-1" ) ) {
+						dynamic_sidebar( "sidebar-1" );
+					}
+					?>
                 </div>
-            <?php endif; ?>
+			<?php endif; ?>
         </div>
     </div>
 
