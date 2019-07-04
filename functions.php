50f0e591927263f2c61a779c3eb6ebdb772c1a85
Imtiaz Epu <imtiazepu@programmer.net>
2019-07-05 04:34:00 +0600

    acf-field group export & acf ship with theme

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 require_once get_theme_file_path('/inc/tgm.php');
+require_once get_theme_file_path('/inc/acf-mb.php');
 if ( class_exists( 'Attachments' ) ) {
 	require_once get_theme_file_path( "/lib/attachments.php" );
 }
@@ -232,6 +233,7 @@ function alpha_modify_main_post($wpq){
     }
 }
 add_action('pre_get_posts', 'alpha_modify_main_post');
+add_filter('acf/settings/show_admin','__return_false');
 
 
 
